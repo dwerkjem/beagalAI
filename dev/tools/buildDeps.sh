30d6d9ff0fa1c06a44074e28354c0bd1deb91a98
dwerkjem <derekrneilson@gmail.com>
2024-04-11 22:31:53 -0600

    Add PROJECT_DIR environment variable and buildDeps.sh script

---
+++ dev/tools/buildDeps.sh
@@ -0,0 +1,8 @@
+if [ -z "$PROJECT_DIR" ]; then
+    echo "PROJECT_DIR is not set. Please set it and rerun the script."
+    exit 1
+fi
+
+# pip freeze
+pip freeze > $PROJECT_DIR/requirements.txt
+echo "requirements.txt has been updated."
