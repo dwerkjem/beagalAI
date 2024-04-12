8aa6f13ed7630cc52588ae0a396748c1281e47d8
dwerkjem <derekrneilson@gmail.com>
2024-04-12 17:23:02 -0600

    Refactor makeWave function in ttsDispatcher.py to improve code readability

--- server/src/modules/ttsDispatcher.py
+++ server/src/modules/ttsDispatcher.py
@@ -2,8 +2,9 @@ from TTS.api import TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
 
 # generate speech by cloning a voice using default settings
-tts.tts_to_file(text="Hello world, today is a beautiful",
-                file_path="output.wav",
-                speaker_wav="in/in.wav",
+def makeWave(text, file_path, speaker_wav):
+    tts.tts_to_file(text=text,
+                file_path=file_path,
+                speaker_wav=speaker_wav,
                 language="en")
 
