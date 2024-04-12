71deb3f7c920c33b00131879c24c12f7a54b2db9
dwerkjem <derekrneilson@gmail.com>
2024-04-12 01:05:51 -0600

    Update buildDeps.sh script to handle missing PROJECT_DIR and pip installation

--- dev/tools/buildDeps.sh
+++ dev/tools/buildDeps.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+
 if [ -z "$PROJECT_DIR" ]; then
     echo "PROJECT_DIR is not set. Please set it and rerun the script."
     exit 1
 fi
 
-# pip freeze
-pip freeze > "$PROJECT_DIR"/requirements.txt
-echo "requirements.txt has been updated."
+if [ ! -d "$PROJECT_DIR" ]; then
+    echo "Error: The directory specified in PROJECT_DIR does not exist."
+    exit 1
+fi
+
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, please install it."
+    exit 1
+fi
+
+
+if pip freeze > "$PROJECT_DIR/requirements.txt"; then
+    echo "requirements.txt has been updated successfully in $PROJECT_DIR."
+else
+    echo "Failed to update requirements.txt."
+    exit 1
+fi
